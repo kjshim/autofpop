9ac4877061f11b4940d020937765691b1e3d0a85
Minwoo Lee <ermaker@gmail.com>
2015-12-08 15:46:17 +0900

    Fix a typo

--- autofpop/make_model.py
+++ autofpop/make_model.py
@@ -9,19 +9,20 @@ def save_model(name, data):
 	recognizer = Recognizer()
 	recognizer.load_data(data)
 	recognizer.model = [RecognizerSplit(), RecognizerDL()]
+	recognizer.model[-1].countof_epoch = 20
 	recognizer.fit()
 	recognizer.model[-1].dump('model/' + name)
 
 if __name__ == '__main__':
 	save_model('color', [
-		'BASE',
-		'FLOWER', 'JAIL', 'SNOW',
-		'STRIPE_1', 'STRIPE_2', 'STRIPE_3',
-		'TRI',
-		])
-	save_model('color', [
 		'BLACK', 'BLUE', 'BROWN', 'GREEN',
 		'PINK', 'WHITE', 'YELLOW',
 		'CONE', 'MAPSCROLL', 'STONE',
 		'NA',
+		])
+	save_model('type', [
+		'BASE',
+		'FLOWER', 'JAIL', 'SNOW',
+		'STRIPE_1', 'STRIPE_2', 'STRIPE_3',
+		'TRI',
 		])
